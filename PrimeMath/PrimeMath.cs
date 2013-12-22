9bf56d6f138aa3ca454c46a59876d78dea7bc6ff
John Gietzen <otac0n@gietzen.us>
2013-12-22 15:38:38 -0800

    Added a sieve implementation.

--- PrimeMath/PrimeMath.cs
+++ PrimeMath/PrimeMath.cs
@@ -9,12 +9,15 @@
 namespace PrimeMath
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Provides static methods for prime-related mathematical functions.
     /// </summary>
     public static class PrimeMath
     {
+        private static readonly PrimeState State = new PrimeState();
+
         /// <summary>
         /// Returns a value that indicates whether the specified value is a composite number.
         /// </summary>
@@ -59,20 +62,7 @@ namespace PrimeMath
         [CLSCompliant(false)]
         public static bool IsPrime(ulong value)
         {
-            if (value <= 1)
-            {
-                return false;
-            }
-
-            for (ulong i = 2; i < value; i++)
-            {
-                if (value % i == 0)
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return State.IsPrime(value);
         }
 
         private static ulong Abs(long value)
@@ -84,6 +74,103 @@ namespace PrimeMath
             else
             {
                 return (ulong)Math.Abs(value);
+            }
+        }
+
+        private class PrimeState
+        {
+            private ulong largestValueChecked;
+            private int nextPrimeSquaredIndex;
+            private List<ulong> primes;
+
+            public PrimeState()
+            {
+                this.primes = new List<ulong> { 2 };
+                this.largestValueChecked = 2;
+                this.nextPrimeSquaredIndex = 0;
+            }
+
+            public void FillPrimesBelow(ulong max)
+            {
+                var nextPrimeSquared = this.primes[this.nextPrimeSquaredIndex];
+                nextPrimeSquared *= nextPrimeSquared;
+
+                for (var num = this.largestValueChecked + 1; num <= max; this.largestValueChecked = num, num++)
+                {
+                    if (num == nextPrimeSquared)
+                    {
+                        var i = this.nextPrimeSquaredIndex + 1;
+                        this.nextPrimeSquaredIndex = i;
+                        nextPrimeSquared = this.primes[i];
+                        nextPrimeSquared *= nextPrimeSquared;
+                        continue;
+                    }
+
+                    var prime = true;
+                    for (var primeIndex = 0; primeIndex < this.nextPrimeSquaredIndex; primeIndex++)
+                    {
+                        if (num % this.primes[primeIndex] == 0)
+                        {
+                            prime = false;
+                            break;
+                        }
+                    }
+
+                    if (prime)
+                    {
+                        this.primes.Add(num);
+                    }
+                }
+            }
+
+            public bool IsPrime(ulong value)
+            {
+                if (this.largestValueChecked >= value)
+                {
+                    return this.primes.BinarySearch(value) >= 0;
+                }
+                else
+                {
+                    var maxFactor = (ulong)Math.Sqrt(value);
+
+                    var primeIndex = 0;
+                    while (true)
+                    {
+                        var prime = this.primes[primeIndex];
+
+                        if (prime > maxFactor)
+                        {
+                            break;
+                        }
+                        else if (value % prime == 0)
+                        {
+                            return false;
+                        }
+
+                        primeIndex++;
+
+                        if (primeIndex >= this.primes.Count)
+                        {
+                            if (this.largestValueChecked >= maxFactor)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                this.FillPrimesBelow(maxFactor);
+
+                                if (primeIndex >= this.primes.Count)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+
+                        prime = this.primes[primeIndex];
+                    }
+
+                    return true;
+                }
             }
         }
     }
